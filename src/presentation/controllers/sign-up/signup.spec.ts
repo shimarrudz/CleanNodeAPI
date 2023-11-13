7cd54d8142d198b7a597754bb15dc17e0418168a
shimarrudz <vic.shima.vss@gmail.com>
2023-11-13 10:35:18 -0300

    refactor: return 500 test

--- src/presentation/controllers/sign-up/signup.spec.ts
+++ src/presentation/controllers/sign-up/signup.spec.ts
@@ -166,8 +166,8 @@ describe('SignUp Controller', () => {
 
   test('Should return 500 if AddAccount throws', async () => {
     const { sut, addAccountStub } = makeSut()
-    jest.spyOn(addAccountStub, 'add').mockImplementationOnce(() => {
-      throw new Error()
+    jest.spyOn(addAccountStub, 'add').mockImplementationOnce(async () => {
+      return await new Promise((resolve, reject) => { reject(new Error()) })
     })
     const httpRequest = {
       body: {
