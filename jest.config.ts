93395c1116157449e9e04fbd389056b832956a5d
shimarrudz <vic.shima.vss@gmail.com>
2024-01-08 06:43:33 -0300

    chore: add jest configs for mongobd

--- jest.config.ts
+++ jest.config.ts
@@ -6,6 +6,7 @@ const config: Config = {
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   testEnvironment: 'node',
+  preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
