898fa3485c50d618e490b5b8882b8b8df6d12f4d
shimarrudz <vic.shima.vss@gmail.com>
2024-01-09 10:31:33 -0300

    test: ensure requests parse body as json

---
+++ src/main/middlewares/body-parser.test.ts
@@ -0,0 +1,14 @@
+import request from 'supertest'
+import app from '../config/app'
+
+describe('Body Parser Middleware', () => {
+  test('Should parse body as json', async () => {
+    app.post('test_body_parser', (req, res) => {
+      res.send()
+    })
+    await request(app)
+      .post('test_body_parser')
+      .send({ name: 'Rodrigo' })
+      .expect({ name: 'Rodrigo' })
+  })
+})
