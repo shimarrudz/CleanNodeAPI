6562746658d34b9e8e550164fc56e64d23333f0a
shima_dev <104167280+shimarrudz@users.noreply.github.com>
2024-01-24 16:13:32 -0300

    fix: wrong file name

--- src/presentation/errors/index.ts
+++ src/presentation/errors/index.ts
@@ -1,4 +1,4 @@
-export * from './invalid-param-errors copy'
+export * from './invalid-param-errors'
 export * from './missing-param-errors'
 export * from './server-error'
 export * from './unauthorized-error'
