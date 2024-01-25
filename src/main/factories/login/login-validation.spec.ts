2351f45962fcfb3566b9c90847cde0c178415afb
shima_dev <104167280+shimarrudz@users.noreply.github.com>
2024-01-25 15:33:35 -0300

    feat: add login validation factory

---
+++ src/main/factories/login/login-validation.spec.ts
@@ -0,0 +1,29 @@
+import { EmailValidation } from '../../../presentation/helpers/validators/email-validation'
+import { RequiredFieldsValidation } from '../../../presentation/helpers/validators/required-field-validation'
+import { type Validation } from '../../../presentation/helpers/validators/validation'
+import { ValidationComposite } from '../../../presentation/helpers/validators/validation-composite'
+import { type EmailValidator } from '../../../presentation/protocols/email-validator'
+import { makeLoginValidation } from './login-validation'
+
+const makeEmailValidator = (): EmailValidator => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid (email: string): boolean {
+      return true
+    }
+  }
+  return new EmailValidatorStub()
+}
+
+jest.mock('../../../presentation/helpers/validators/validation-composite')
+
+describe('LoginValidation Factory', () => {
+  test('Should call ValidationComposite with all validations', () => {
+    makeLoginValidation()
+    const validations: Validation[] = []
+    for (const field of ['email', 'password']) {
+      validations.push(new RequiredFieldsValidation(field))
+    }
+    validations.push(new EmailValidation('email', makeEmailValidator()))
+    expect(ValidationComposite).toHaveBeenCalledWith(validations)
+  })
+})
