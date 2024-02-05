64bef07adfeaa33d1259bf033d7fc36cf252bbbe
shima_dev <104167280+shimarrudz@users.noreply.github.com>
2024-02-05 19:41:47 -0300

    feat: add LoginFactory

--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { type Router } from 'express'
-import { makeSignUpController } from '../factories/signup/signup'
+import { makeSignUpController } from '../factories/signup/signup-factory'
 import { adaptRoute } from '../adapters/express/express-route-adapter'
 
 export default (router: Router): void => {
